3b256d08ddcdcdec5aeee4fdf9082a1b730c71f7
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2026-01-08 10:26:13 +0900

    Add rustc compilation test
    - Add test that compiles and runs a simple Rust hello world program
    - Verifies that rustc can compile from stdin and produce working executables

--- test/runtests.jl
+++ test/runtests.jl
@@ -15,6 +15,15 @@ end
     @test success(`$(rustc()) --version`)
 end
 
+@testset "rustc hello.rs" begin
+    mktempdir() do dir
+        cd(dir) do
+            run(pipeline(`sh -lc "echo 'fn main(){println!(\"Hello, world!\");}' | $(rustc()) -o hello - && ./hello"`))
+            @test success(`./hello`)
+        end
+    end
+end
+
 @testset "cargo run with examples/hello" begin
     cd(joinpath(pkgdir(RustToolChain), "examples", "hello")) do
         @test success(`$(cargo()) run`)
